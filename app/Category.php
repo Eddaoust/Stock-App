98f00316c934475f46f0def50f47d6c868cabacf
Eddaoust <edmond.daoust@gmail.com>
2019-08-09 15:20:20 +0200

    Add subcategories in model

--- app/Category.php
+++ app/Category.php
@@ -11,4 +11,14 @@ class Category extends Model
     public function products() {
         return $this->hasMany('App\Product');
     }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Category', 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Category', 'parent_id');
+    }
 }
